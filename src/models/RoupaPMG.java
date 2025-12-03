2d8caf6f69f47b1aba62f246eb75ef572159e08a
Andrey Soares <77845090+Ar3sMyth@users.noreply.github.com>
2025-12-02 21:55:46 -0300

    Update RoupaPMG.java

--- src/models/RoupaPMG.java
+++ src/models/RoupaPMG.java
@@ -1,5 +1,76 @@
 package models;
 
-public class RoupaPMG {
+import java.util.Scanner;
 
+public class RoupaPMG implements Item {
+    Scanner sc = new Scanner(System.in);
+    String descricao;
+    int quantidadeP;
+    int quantidadeM;
+    int quantidadeG;
+    int estoqueMinimo;
+    int estoqueMaximo;
+
+    public RoupaPMG(String descricao, int quantidadeP, int quantidadeM, int quantidadeG, 
+                    int estoqueMinimo, int estoqueMaximo) { 
+        this.descricao = descricao;
+        this.quantidadeP = quantidadeP;
+        this.quantidadeM = quantidadeM;
+        this.quantidadeG = quantidadeG;
+        this.estoqueMinimo = estoqueMinimo;
+        this.estoqueMaximo = estoqueMaximo;
+    }
+
+    @Override
+    public void venda() {
+        System.out.println("Tamanho do Item: ");
+        String tamanho = sc.next();
+        tamanho = tamanho.toUpperCase(); 
+        switch (tamanho) {
+            case "P":
+                this.quantidadeP -= 1;
+                break;
+            case "M": 
+                this.quantidadeM -= 1;
+                break;
+            case "G":
+                this.quantidadeG -= 1;
+                break;
+        }
+    }
+
+    @Override
+    public void reposicaoEstoque() {
+        if (quantidadeP < estoqueMinimo) {
+            quantidadeP = estoqueMaximo;
+        } 
+        if (quantidadeM < estoqueMinimo) {
+            quantidadeM = estoqueMaximo;
+        }
+        if (quantidadeG < estoqueMinimo) {
+            quantidadeG = estoqueMaximo;
+        }
+    }
+
+    @Override
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public int getQuantidadeP() {
+        return this.quantidadeP;
+    }
+
+    public int getQuantidadeM() {
+        return this.quantidadeM;
+    }
+
+    public int getQuantidadeG() {
+        return this.quantidadeG;
+    }
+
+    @Override
+    public int getQuantidade() {
+        return this.quantidadeG + this.quantidadeM + this.quantidadeP;
+    }
 }
