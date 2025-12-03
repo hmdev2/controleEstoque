f423f915892858e040a825c87fc7ca3b29626837
Andrey Soares <77845090+Ar3sMyth@users.noreply.github.com>
2025-12-02 21:54:44 -0300

    Update Acessorio.java

--- src/models/Acessorio.java
+++ src/models/Acessorio.java
@@ -1,5 +1,23 @@
 package models;
 
-public class Acessorio {
+import java.util.Scanner;
 
+public class Acessorio extends Peca implements Item {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public Acessorio(String descricao, int quantidade, int estoqueMinimo, int estoqueMaximo) {
+        super(descricao, quantidade, estoqueMinimo, estoqueMaximo);
+    }
+
+    @Override
+    public void venda() {
+        System.out.print("Quantidade vendida do acessório: ");
+        int qtd = scanner.nextInt();
+        if (qtd <= quantidade) {
+            quantidade -= qtd;
+        } else {
+            System.out.println("Quantidade insuficiente em estoque.");
+        }
+    }
+    
 }
